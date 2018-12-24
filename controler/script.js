578571766c97b9b85205e4966cada6c53890189b
jackson <github.com/Consultview/HappyClient>
2018-12-23 23:29:48 -0300

    corrigido o erro do click no voto que aparecia string e nao numero

--- controler/script.js
+++ controler/script.js
@@ -1,48 +1,55 @@
 
+var contador1 = 1;
+var contador2 = 1;
+var contador3 = 1;
+var contador4 = 1;
+var contador5 = 1;
+
 function votoExcelente(){
-    document.getElementById("idVotoExcelente");
-    document.getElementById("contador1").innerHTML = contador1 += 1;
-}
+	
+	document.getElementById("contador1").innerHTML = contador1 ++;
+
+};
+
 
 function votoBom(){
 	/*document.getElementById("idVotoBom");*/
-	document.getElementById("contador2").innerHTML = contador2 += 1;	
-}
+	document.getElementById("contador2").innerHTML = contador2 ++;	
+};
 
 function votoRegular(){
-	document.getElementById("idVotoRegular");
-	document.getElementById("contador3").innerHTML = contador3 += 1;	
-}
+	
+	document.getElementById("contador3").innerHTML = contador3 ++;	
+};
 
 function votoRuim(){
-	document.getElementById("idVotoRuim");
-	document.getElementById("contador4").innerHTML = contador4 += 1;
-}
+	
+	document.getElementById("contador4").innerHTML = contador4 ++;	
+};
 
 function votoPessimo(){
-    document.getElementById("idVotoPessimo");
-	document.getElementById("contador5").innerHTML = contador5 += 1;	
-}
+    
+	document.getElementById("contador5").innerHTML = contador5 ++;	
+};
 
-
-
+//funcoes que limpa o voto
 
 function botao1(){
-    document.getElementById('contador1').innerHTML = contador1 = -1;  
-}
+   document.getElementById('contador1').innerHTML = contador1 = 0;  	
+};
 
 function botao2(){
-	document.getElementById('contador2').innerHTML = contador2 = -1;	
-}
+	document.getElementById('contador2').innerHTML = contador2 = 0;	
+};
 
 function botao3(){
-	document.getElementById('contador3').innerHTML = contador3 = -1;	
-}
+	document.getElementById('contador3').innerHTML = contador3 = 0;	
+};
 
 function botao4(){
-	document.getElementById('contador4').innerHTML = contador4 = -1;	
-}
+	document.getElementById('contador4').innerHTML = contador4 = 0;	
+};
 
 function botao5(){
-	document.getElementById('contador5').innerHTML = contador5 = -1;	
-}
+	document.getElementById('contador5').innerHTML = contador5 = 0;	
+};
